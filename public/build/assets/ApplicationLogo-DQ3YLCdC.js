2b21712763ab0c88902cec9f220e33f176220396
Karim Fayek Malak <30074501+karimfayek@users.noreply.github.com>
2026-01-06 13:17:51 +0200

    cars2

--- public/build/assets/ApplicationLogo-DQ3YLCdC.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-eBe_06IG.js";function i(o){return r.jsx("img",{...o,src:"/logo.webp"})}export{i as A};
---
+++ public/build/assets/ApplicationLogo-DdsMb_6p.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-Bspg5fep.js";function i(o){return r.jsx("img",{...o,src:"/logo.webp"})}export{i as A};
